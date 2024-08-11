d1f57aa7085d3bc24a6a395a8770fa90da57ced8
cplaza <5412175+cplaza0997@users.noreply.github.com>
2024-08-11 11:10:51 -0700

    CI/CD

--- app/test/test_api.py
+++ app/test/test_api.py
@@ -4,12 +4,12 @@ import json
 client = TestClient(router)
 
 def test_sum_numbers():
-    response = client.post("/sum", json={"a": 1, "b": 2})
+    response = client.post("/arit", json={"o":"+","a": 1, "b": 2})
     assert response.status_code == 200
     assert response.json() == {"result": 3}
     
 def test_sum_numbers_negative():
-    response = client.post("/sum", json={"a": -1, "b": -2})
+    response = client.post("/arit", json={"o":"+","a": -1, "b": -2})
     assert response.status_code == 200
     assert response.json() == {"result": -3}
 
